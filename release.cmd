e302d0edfab8c0abb48b3a0c5597da879176729e
Chen Bear <chenll7@outlook.com>
2023-09-15 23:19:02 +0800

    Some change.

--- release.cmd
+++ release.cmd
@@ -2,11 +2,14 @@
 
 rmdir /s/q package
 
+echo Donwload dependencies.
 call py -m pip download -d package .
 
+echo Packaging...
 call py -m pip wheel -w package .
 
-echo Delete ../../../tool-homemade/bear_demo/package
+echo Delete ../../../tool-homemade/bear_demo/package .
 rmdir /s/q "../../../tool-homemade/bear_demo/package"
 
+echo Copy packages to ../../../tool-homemade/bear_demo/package .
 Xcopy "package" "../../../tool-homemade/bear_demo/package" /E /H /I
