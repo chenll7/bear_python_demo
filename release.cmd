b6bda24a7ef845c8b7784be7a38fdb1557584127
Chen Bear <chenll7@outlook.com>
2023-09-16 09:39:34 +0800

    Some change.

--- release.cmd
+++ release.cmd
@@ -10,12 +10,12 @@ echo Packaging...
 call py -m pip wheel -w package .
 echo=
 
-echo Deleting ../../../tool-homemade/bear_demo/package ...
-rmdir /s/q "../../../tool-homemade/bear_demo/package"
+echo Deleting ../../../tool-homemade/bear_python_demo/package ...
+rmdir /s/q "../../../tool-homemade/bear_python_demo/package"
 echo=
 
 timeout /nobreak /t 3
 
-echo Copying packages to ../../../tool-homemade/bear_demo/package ...
-Xcopy "package" "../../../tool-homemade/bear_demo/package" /E /H /I
+echo Copying packages to ../../../tool-homemade/bear_python_demo/package ...
+Xcopy "package" "../../../tool-homemade/bear_python_demo/package" /E /H /I
 echo=
