f31fede3cfd981230c85bbb6d9756a63bbcc4544
Chen Bear <chenll7@outlook.com>
2024-01-07 15:20:20 +0800

    Some changes.

--- tool/helper/release.py
+++ tool/helper/release.py
@@ -2,14 +2,14 @@ import subprocess
 import shlex
 import shutil
 import os
-from os import path
 import tomllib
+from pathlib import Path
 
 
-PACKAGE_FOLDER_PATH = "package"
-BUILD_FOLDER_PATH = "build"
-RELEASE_FOLDER_PATH = "release"
-INST_TMPL_FOLDER_PATH = "installer_template"
+PACKAGE_FOLDER_PATH = Path("package")
+BUILD_FOLDER_PATH = Path("build")
+RELEASE_FOLDER_PATH = Path("release")
+INST_TMPL_FOLDER_PATH = Path("installer_template")
 
 
 def run(cmd):
@@ -23,26 +23,32 @@ with open("pyproject.toml", "r") as pyproject_file:
 project_name = pyproject["project"]["name"]
 print(f"Reading project name is down. Project name is {project_name} .\n")
 
-installer_path = path.join(RELEASE_FOLDER_PATH, project_name)
+installer_dir_path = Path(RELEASE_FOLDER_PATH, project_name)
+installer_path = Path(RELEASE_FOLDER_PATH, project_name)
 
 run(rf'"tool\package.cmd"')
 
 print(f"Recreating the folder {RELEASE_FOLDER_PATH} ...")
-if path.exists(RELEASE_FOLDER_PATH):
+if RELEASE_FOLDER_PATH.exists():
     shutil.rmtree(RELEASE_FOLDER_PATH)
-os.mkdir(RELEASE_FOLDER_PATH)
+RELEASE_FOLDER_PATH.mkdir(parents=True, exist_ok=True)
 print(f"Recreating the folder {RELEASE_FOLDER_PATH} is done.\n")
 
-print("Creating the installer...")
-shutil.copytree(INST_TMPL_FOLDER_PATH, installer_path)
+print("Creating the installer folder...")
+shutil.copytree(INST_TMPL_FOLDER_PATH, installer_dir_path)
 shutil.copytree(
-    PACKAGE_FOLDER_PATH, path.join(installer_path, "package")
+    PACKAGE_FOLDER_PATH, Path(installer_dir_path, "package")
 )
-print("Creating the installer is done.\n")
+print("Creating the installer folder is done.\n")
+
+print("Packaging the installer...")
+print(installer_path)
+shutil.make_archive(installer_path, 'zip', installer_dir_path)
+print("Packaging the installer is done.\n")
 
 print("Creating the updater...")
 initial_cwd = os.getcwd()
-os.chdir(installer_path)
+os.chdir(installer_dir_path)
 run('py "helper/script/create_updater.py" for-program')
 os.chdir(initial_cwd)
 print("Creating the updater is done.")
