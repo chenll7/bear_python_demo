2a1e460559e23c2b653ce42e0d24e7bf4d635c7a
Chen Bear <chenll7@outlook.com>
2024-01-27 22:30:02 +0800

    Some changes.

--- installer_template/start.cmd
+++ installer_template/start.cmd
@@ -1,3 +1,9 @@
 @echo off
 
-call "venv/Scripts/python" -m bear_python_demo.entry main2
+if not exist "venv/Scripts/python.exe" (
+    echo venv/Scripts/python.exe not found!
+    pause
+    exit
+)
+
+call "venv/Scripts/python" -m bear_python_demo.entry main
